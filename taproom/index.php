6a75c2b07eb1623a920651aa388f1d39047f3a49
Prashant Jadhav <prashant.j@peopleinteractive.in>
2017-12-06 12:28:22 +0530

    taproom static single page site added

---
+++ taproom/index.php
@@ -0,0 +1,169 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1, maximum-scale=1.0, minimal-ui">
+<title>Getway Taproom</title>
+<link href="css/style.css" media="screen" rel="stylesheet" type="text/css" />
+<link href="css/slick.css" media="screen" rel="stylesheet" type="text/css" />
+<link href="css/slick-theme.css" media="screen" rel="stylesheet" type="text/css" />
+<link href='https://fonts.googleapis.com/css?family=Roboto:400,700' rel='stylesheet' type='text/css'>
+</head>
+<body>
+<div class="your-class">
+    <div><img src="images/assest-1.jpg"></div>
+    <div><img src="images/assest-2.jpg"></div>
+    <div><img src="images/assest-3.jpg"></div>
+    <div><img src="images/assest-4.jpg"></div>
+    <div><img src="images/assest-5.jpg"></div>
+    <div><img src="images/assest-6.jpg"></div>
+    <div><img src="images/assest-7.jpg"></div>
+    <div><img src="images/assest-8.jpg"></div>
+    <div><img src="images/assest-9.jpg"></div>
+    <div><img src="images/assest-10.jpg"></div>
+    <div><img src="images/assest-11.jpg"></div>
+  </div>
+<div class="eName">
+  <span>Gateway Taproom, BKC</span>
+
+  <div style="padding:5px"></div>
+
+  <span style="margin-right:5px" class="rtng" aria-hidden="true">4.4</span>
+  <span class="_pxg" aria-label="Rated 4.4 out of 5,">
+    <span class="rating" style="width:63px"></span>
+  </span>
+  <div style="display:inline;font-size:14px;margin-left:3px">
+    <span class="rtng" style="white-space:nowrap" aria-label="89 reviews">(89)</span>
+  </div>
+</div>
+
+<div class="eDetails">
+  <span class="icons">
+    <svg focusable="false" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+      <path d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z"></path>
+    </svg>
+  </span>
+  <span style="display:table-cell">Godrej BKC Building, Ground Floor, Unit no. 3, G Block, Bandra Kurla Complex (E), Mumbai, Maharashtra 400051</span>
+</div>
+
+<div class="eDetails">
+  <span class="icons">
+    <svg focusable="false" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+      <path d="M11.99 2C6.47 2 2 6.48 2 12s4.47 10 9.99 10C17.52 22 22 17.52 22 12S17.52 2 11.99 2zM12 20c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8zm.5-13H11v6l5.25 3.15.75-1.23-4.5-2.67z"></path>
+    </svg>
+  </span>
+  <span class="show">
+    Open Monday - Sunday 12PM-1:30AM
+  </span>
+</div>
+
+<div class="eDetails">
+  <span class="menu"></span>
+  <span class="show">Menu&nbsp; &nbsp;<a href="http://www.gatewaybrewery.com/gateway-taproom-menu/" target="_blank">gatewaybrewery.com</a></span>
+</div>
+
+<div class="eDetails">
+  <span class="icons">
+    <svg focusable="false" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+      <path d="M17 12h-5v5h5v-5zM16 1v2H8V1H6v2H5c-1.11 0-1.99.9-1.99 2L3 19c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2h-1V1h-2zm3 18H5V8h14v11z"></path>
+    </svg>
+  </span>
+  <span class="show">
+    Find a table&nbsp; &nbsp;<a href="https://www.dineout.co.in/mumbai/gateway-taproom-bandra-east-bandra-34535" target="_blank">dineout.co.in</a></span>
+  </span>
+</div>
+
+<div class="eDetails">
+  <span class="icons">
+    <svg focusable="false" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+      <path d="M20.01 15.38c-1.23 0-2.42-.2-3.53-.56a.977.977 0 0 0-1.01.24l-1.57 1.97c-2.83-1.35-5.48-3.9-6.89-6.83l1.95-1.66c.27-.28.35-.67.24-1.02-.37-1.11-.56-2.3-.56-3.53 0-.54-.45-.99-.99-.99H4.19C3.65 3 3 3.24 3 3.99 3 13.28 10.73 21 20.01 21c.71 0 .99-.63.99-1.18v-3.45c0-.54-.45-.99-.99-.99z"></path>
+    </svg>
+  </span>
+  <span class="show">
+    022 2653 4748
+  </span>
+</div>
+
+<!-- <div class="eDetails">
+  <div class="highlights">
+    Highlights
+  </div>
+  <div class="r-iZPCHQFHxRP8">
+    <div style="padding:0 5px 0 24px;width:87px;display:inline-block;margin-left:auto;margin-right:auto;text-align:center;vertical-align:top;white-space:normal">
+      <div style="height:48px;margin-left:auto;margin-right:auto;width:48px;">
+        <div style="height:34px;margin-left:auto;margin-right:auto;padding-top:7px;width:34px;">
+          <svg style="width:34px;height:34px;" viewBox="8.5 8.5 30 30">
+            <path d="M14 23.63v-5.76c0-1.58 1.29-2.87 2.87-2.87h14.26c1.58 0 2.87 1.29 2.87 2.87v5.76c0 1.58-1.29 2.87-2.87 2.87H16.87c-1.58 0-2.87-1.29-2.87-2.87z" fill="#DA4336"></path>
+            <path d="M32.75 24h-17.5c-.96 0-1.75.79-1.75 1.75s.79 1.75 1.75 1.75h17.5c.96 0 1.75-.79 1.75-1.75S33.71 24 32.75 24z" fill="#EA7A72"></path><path d="M13.09 28.9c0-.9.74-1.64 1.64-1.64h18.55c.9 0 1.64.74 1.64 1.64 0 .9-.74 1.64-1.64 1.64H14.73c-.9-.01-1.64-.74-1.64-1.64z" fill="#B7251D"></path>
+            <circle cx="28.36" cy="19.62" fill="#FBC730" r="1.09"></circle>
+            <circle cx="19.09" cy="19.62" fill="#FBC730" r="1.09"></circle>
+            <path d="M34.5 33c-.83 0-1.5-.67-1.5-1.5v-9c0-.82.67-1.5 1.5-1.5s1.5.67 1.5 1.5v9c0 .83-.67 1.5-1.5 1.5zM13.5 33c-.83 0-1.5-.67-1.5-1.5v-9c0-.82.67-1.5 1.5-1.5.82 0 1.5.67 1.5 1.5v9c0 .83-.67 1.5-1.5 1.5z" fill="#D31C1C"></path>
+          </svg>
+        </div>
+      </div>
+      <span style="-webkit-box-orient:vertical;color:#757575;display:-webkit-inline-box;font-size:12px;-webkit-line-clamp:3;line-height:16px;overflow:hidden;text-align:center;word-break:break-word">Cosy</span>
+    </div>
+    <div style="padding:0 5px 0 0;width:87px;display:inline-block;margin-left:auto;margin-right:auto;text-align:center;vertical-align:top;white-space:normal">
+      <div style="height:48px;margin-left:auto;margin-right:auto;width:48px;">
+        <div style="height:34px;margin-left:auto;margin-right:auto;padding-top:7px;width:34px;">
+        <svg style="width:34px;height:34px;" viewBox="8.5 8.5 30 30">
+          <g fill="#0E9C57">
+            <path d="M18.04 35C15.26 35 13 32.74 13 29.96V18.04c0-2.78 2.26-5.04 5.04-5.04 2.78 0 5.04 2.26 5.04 5.04v11.92c0 2.78-2.25 5.04-5.04 5.04zM29.96 35c-2.78 0-5.04-2.26-5.04-5.04V18.04c0-2.78 2.26-5.04 5.04-5.04 2.78 0 5.04 2.26 5.04 5.04v11.92c0 2.78-2.26 5.04-5.04 5.04z"></path>
+          </g>
+          <path d="M17.1 16.37l3.42 7.23c.24.5.83.71 1.33.48.5-.24.71-.83.48-1.33l-3.42-7.23c-.24-.5-.83-.71-1.33-.48-.51.23-.72.83-.48 1.33z" fill="#F3B300"></path>
+          <path d="M18.9 16.37l-3.42 7.23c-.24.5-.83.71-1.33.48-.5-.24-.71-.83-.48-1.33l3.42-7.23c.24-.5.83-.71 1.33-.48.51.23.72.83.48 1.33z" fill="#FFE299"></path>
+          <path d="M29.02 16.37l3.42 7.23c.24.5.83.71 1.33.48.5-.24.71-.83.48-1.33l-3.42-7.23c-.24-.5-.83-.71-1.33-.48-.5.23-.71.83-.48 1.33z" fill="#F3B300"></path>
+          <path d="M30.83 16.37L27.4 23.6c-.24.5-.83.71-1.33.48-.5-.24-.71-.83-.48-1.33l3.42-7.23c.24-.5.83-.71 1.33-.48.51.23.73.83.49 1.33z" fill="#FFE299"></path>
+        </svg>
+      </div>
+    </div>
+    <span style="-webkit-box-orient:vertical;color:#757575;display:-webkit-inline-box;font-size:12px;-webkit-line-clamp:3;line-height:16px;overflow:hidden;text-align:center;word-break:break-word">Casual</span>
+  </div>
+  <div style="padding:0 24px 0 0;width:87px;display:inline-block;margin-left:auto;margin-right:auto;text-align:center;vertical-align:top;white-space:normal">
+    <div style="height:48px;margin-left:auto;margin-right:auto;width:48px;">
+        <div style="height:34px;margin-left:auto;margin-right:auto;padding-top:7px;width:34px;">
+        <svg style="width:34px;height:34px;" viewBox="8.5 8.5 30 30">
+          <path d="M36 31h-8v-4c0-2.21 1.79-4 4-4s4 1.79 4 4v4z" fill="#4184F3"></path>
+          <path d="M28 31h-8v-4c0-2.21 1.79-4 4-4s4 1.79 4 4v4z" fill="#0E9C57"></path>
+          <path d="M20 31h-8v-4c0-2.21 1.79-4 4-4s4 1.79 4 4v4z" fill="#DA4336"></path>
+          <circle cx="32" cy="19" fill="#F3B300" r="3"></circle>
+          <circle cx="24" cy="19" fill="#F3B300" r="3"></circle>
+          <circle cx="16" cy="19" fill="#F3B300" r="3"></circle>
+        </svg>
+      </div>
+    </div>
+    <span style="-webkit-box-orient:vertical;color:#757575;display:-webkit-inline-box;font-size:12px;-webkit-line-clamp:3;line-height:16px;overflow:hidden;text-align:center;word-break:break-word">Good for groups</span>
+  </div>
+</div> -->
+<div class="eDetails">
+  <div class="highlights">
+    reviews from the web
+  </div>
+  <div style="margin: 0 8px">
+    <div style="padding-bottom:24px;display:table;table-layout:fixed;width:100%">
+      <a style="display:table-cell;color:#4285f4;width:33.3333%;box-sizing;border-box;border-right:1px solid rgba(0, 0, 0, .16);padding:0 8px" aria-label="Gateway Taproom, BKC got a 4.3 stars review on Zomato." href="https://www.zomato.com/mumbai/gateway-taproom-bandra-kurla-complex"><span style="line-height:16px;padding-top:3px;display:block;text-align:center;white-space:normal;font-size:14px;overflow:hidden;text-overflow:ellipsis;" aria-label="Gateway Taproom, BKC got a 4.3 stars review on Zomato." title="Zomato">Zomato</span><span style="line-height:21px;display:block;text-align:center;color:rgba(0,0,0,.87);font-size:16px;font-weight:400;margin-top:5px;overflow:hidden;text-overflow:ellipsis;white-space:nowrap;" aria-label="Gateway Taproom, BKC got a 4.3 stars review on Zomato.">4.3/5</span><span style="display:block;text-align:center;color:#AAA;font-size:14px;overflow:hidden;text-overflow:ellipsis;white-space:nowrap;" aria-label="Gateway Taproom, BKC got a 4.3 stars review on Zomato.">105 votes</span></a>
+      <a style="display:table-cell;color:#4285f4;width:33.3333%;box-sizing;border-box;border-right:1px solid rgba(0, 0, 0, .16);padding:0 8px" aria-label="Gateway Taproom, BKC got a 4.5 stars review on Dineout." href="https://www.dineout.co.in/mumbai/gateway-taproom-bandra-east-bandra-34535"><span style="line-height:16px;padding-top:3px;display:block;text-align:center;white-space:normal;font-size:14px;overflow:hidden;text-overflow:ellipsis;" aria-label="Gateway Taproom, BKC got a 4.5 stars review on Dineout." title="Dineout">Dineout</span><span style="line-height:21px;display:block;text-align:center;color:rgba(0,0,0,.87);font-size:16px;font-weight:400;margin-top:5px;overflow:hidden;text-overflow:ellipsis;white-space:nowrap;" aria-label="Gateway Taproom, BKC got a 4.5 stars review on Dineout.">4.5/5</span><span style="display:block;text-align:center;color:#AAA;font-size:14px;overflow:hidden;text-overflow:ellipsis;white-space:nowrap;" aria-label="Gateway Taproom, BKC got a 4.5 stars review on Dineout.">12 votes</span></a>
+      <a style="display:table-cell;color:#4285f4;width:33.3333%;box-sizing;border-box;padding:0 8px" aria-label="Gateway Taproom, BKC got a 4.8 stars review on Facebook." href="https://m.facebook.com/GatewayTaproom/"><span style="line-height:16px;padding-top:3px;display:block;text-align:center;white-space:normal;font-size:14px;overflow:hidden;text-overflow:ellipsis;" aria-label="Gateway Taproom, BKC got a 4.8 stars review on Facebook." title="Facebook">Facebook</span><span style="line-height:21px;display:block;text-align:center;color:rgba(0,0,0,.87);font-size:16px;font-weight:400;margin-top:5px;overflow:hidden;text-overflow:ellipsis;white-space:nowrap;" aria-label="Gateway Taproom, BKC got a 4.8 stars review on Facebook.">4.8/5</span><span style="display:block;text-align:center;color:#AAA;font-size:14px;overflow:hidden;text-overflow:ellipsis;white-space:nowrap;" aria-label="Gateway Taproom, BKC got a 4.8 stars review on Facebook.">55 votes</span></a>
+    </div>
+  </div>
+</div>
+<script src="js/jquery-1.11.3.min.js" type="text/javascript"></script>
+<script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+<script type="text/javascript" src="/js/slick/slick.js"></script>
+
+  <script type="text/javascript">
+    $(document).ready(function(){
+      $('.your-class').slick({
+        lazyLoad: 'ondemand',
+        adaptiveHeight: true,
+        dots: true,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear',
+        arrows: true
+      });
+    });
+  </script>
+</body>
+</html>
